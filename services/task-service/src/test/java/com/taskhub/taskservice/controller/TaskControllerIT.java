75d6367798ab67a9738cb8d95768abfaf0d2652a
Tajinder Pal Singh <tajinderpalsingh@mac.speedport.ip>
2026-01-23 11:29:06 +0100

    test container properties

--- services/task-service/src/test/java/com/taskhub/taskservice/controller/TaskControllerIT.java
+++ services/task-service/src/test/java/com/taskhub/taskservice/controller/TaskControllerIT.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -33,10 +35,19 @@ class TaskControllerIT {
 
     @Autowired
     private TaskRepository repository;
-    
+
     @Container
     static PostgreSQLContainer<?> postgres =
             new PostgreSQLContainer<>("postgres:15");
+
+
+
+    @DynamicPropertySource
+    static void registerProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @Test
     void fullTaskLifecycle_shouldWork() throws Exception {
